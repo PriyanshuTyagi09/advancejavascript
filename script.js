dcaef2461a3a66673a2142297156b32f2c7eae25
PriyanshuT09 <priyanshutyagi1984@gmail.com>
2025-04-02 16:25:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// function sayHello() {
+//     let you = prompt("What's your name?");
+//     console.log("Hello", you + "!");
+// }
+// sayHello();
+
+// let varContainingFunction = function(){
+//     let varInFunction = "I'm in a function.";
+//     console.log("hi there!", varInFunction)
+// }
+// varContainingFunction();
+
+// function tester(para1,para2){
+//     return para1 + " " + para2;
+// }
+// const arg1= "argumnet 1";
+// const arg2= "argument 2";
+// console.log(tester(arg1, arg2));
+
+function addTwoNumbers(x=2,y=3) {
+    console.log(x+y);
+}
+addTwoNumbers(1,2,3,4);
+//default arguments above mentioned as x and y are set by default. can make only one of them default instead of both.
+// addTwoNumbers(22,44)
+// num1=parseInt(prompt("Enter first number"));
+// num2=parseInt(prompt("enter second number"));
+// addTwoNumbers(num1,num2)
+
+
